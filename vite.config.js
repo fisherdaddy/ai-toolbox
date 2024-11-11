d4d4aae9afc60793e429a952ffbfaaae593dd03f
fisherdaddy <wn0808040236@163.com>
2024-11-11 09:53:53 +0800

    chore: rm useless css & update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,6 @@ import sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    sitemap({
-      // plugin options
-    }),
-  ],
   server: {
     port: 3000
   }
